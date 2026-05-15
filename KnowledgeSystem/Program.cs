9b2f963dcbe155ada6831c64e77e98ede51d2656
empireu <rodney.mckay.meredith@gmail.com>
2026-05-15 20:03:36 +0300

    Sets up Netcord

--- KnowledgeSystem/Program.cs
+++ KnowledgeSystem/Program.cs
@@ -1,14 +1,10 @@
-using System.ClientModel;
-using KnowledgeSystem;
-using KnowledgeSystem.Agent;
-using KnowledgeSystem.Agents.Context;
-using KnowledgeSystem.Agents.Context.TokenEstimation;
-using KnowledgeSystem.Agents.Orchestration;
+using KnowledgeSystem.Discord;
 using KnowledgeSystem.Retrieval;
 using KnowledgeSystem.Retrieval.Engine;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OpenAI;
+using NetCord.Hosting.Gateway;
+using NetCord.Hosting.Services.ApplicationCommands;
 using Serilog;
 
 var builder = Host.CreateDefaultBuilder(args)
@@ -22,6 +18,11 @@ var builder = Host.CreateDefaultBuilder(args)
             .ReadFrom.Configuration(context.Configuration)
             .Enrich.FromLogContext()
             .WriteTo.Console();
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddDiscordGateway();
+        services.AddApplicationCommands();
     });
 
 var host = builder.Build();
@@ -29,58 +30,6 @@ var host = builder.Build();
 var engine = host.Services.GetRequiredService<RagEngine>();
 await engine.InitializeAsync();
 
-var agent = new SimpleChatAgent("chat", host.Services);
-var context = new SimpleChatContext();
-context.ChatContext.InsertSystem(File.ReadAllText("sp.md"));
-var tokenizer = TokenizerHelper.Create(TokenizerInfo.Gemma("tokenizer/gemma-4"));
+host.AddApplicationCommandModule<TestModule>();
 
-var observer = new Observer(tokenizer);
-
-var key = File.Exists("key.txt") ? File.ReadAllText("key.txt").Trim() : "none";
-var client = new OpenAIClient(
-    new ApiKeyCredential(key),
-    new OpenAIClientOptions
-    {
-        Endpoint = new Uri("http://127.0.0.1:1234/v1")
-    } 
-).GetChatClient("google/gemma4-e4b");
-
-var compactor = new ContextCompactor(tokenizer, client, maxContextTokens: 14000);
-
-Console.WriteLine("Ready\n");
-
-while (true)
-{
-    Console.Write($"({tokenizer.CountTokens(context.ChatMessages)} tokens) > ");
-    var input = Console.ReadLine();
-    
-    if (string.IsNullOrWhiteSpace(input))
-    {
-        break;
-    }
-
-    context.ChatContext.InsertUser(input);
-
-    var runner = new AgentRunner<SimpleChatContext>(
-        observer,
-        client,
-        agent,
-        null,
-        context,
-        CancellationToken.None
-    );
-
-    while (true)
-    {
-        var turnResult = await runner.ExecuteTurn();
-        
-        Console.WriteLine($"Turn: {turnResult}");
-
-        if (runner.IsFinished)
-        {
-            break;
-        }
-    }
-
-    await compactor.CompactAsync(context.ChatContext);
-}
+await host.RunAsync();
