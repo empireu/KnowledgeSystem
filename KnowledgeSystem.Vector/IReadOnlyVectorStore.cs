b423ccdfb62875c0902ab928acce887e8717b8e2
empireu <rodney.mckay.meredith@gmail.com>
2026-05-25 15:54:35 +0300

    Creates new folder for vector search and adds two interfaces for future work

---
+++ KnowledgeSystem.Vector/IReadOnlyVectorStore.cs
@@ -0,0 +1,16 @@
+﻿namespace KnowledgeSystem.Vector;
+
+public interface IReadOnlyVectorStore
+{
+    /// <summary>
+    ///     Gets the dimension of the vector space.
+    /// </summary>
+    public int Dimension { get; }
+    
+    /// <summary>
+    ///     Searches for the approximate <see cref="k"/> vectrs most similar to <see cref="query"/>.
+    /// </summary>
+    /// <param name="query">A vector matching the <see cref="Dimension"/>.</param>
+    /// <param name="k">The maximum number of vectors to explore.</param>
+    public VectorSearchResult[] Search(ReadOnlySpan<float> query, int k);
+}
---
+++ KnowledgeSystem.Vector/IVectorStore.cs
@@ -0,0 +1,9 @@
+﻿namespace KnowledgeSystem.Vector;
+
+public interface IVectorStore : IReadOnlyVectorStore
+{
+    /// <summary>
+    ///     Inserts a new vector into the database. Guaranteed to be thread-safe.
+    /// </summary>
+    public IStoredVector Insert(float[] data);
+}
